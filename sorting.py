4e95aff65d8d933257e97305e777361b5616c196
246994 <246994@vutbr.cz>
2025-04-14 10:12:38 +0200

    (insert)

--- sorting.py
+++ sorting.py
@@ -35,13 +35,12 @@ def selection_sort(seznam,direction = "asc"):
             if direction == "asc":
                 if seznam[j] < seznam[min_indx]:
                     min_indx = j
-                    seznam[i], seznam[min_indx] = seznam[min_indx], seznam[i]
+
             elif direction == "desc":
-                max_indx = i
-                for j in range(i+1,delka):
-                    if seznam[j] > seznam[max_indx]:
-                        max_indx = j
-                seznam[i],seznam[max_indx] = seznam[max_indx],seznam[i]
+                    if seznam[j] > seznam[min_indx]:
+                        min_indx = j
+
+        seznam[i],seznam[min_indx] = seznam[min_indx],seznam[i]
     return seznam
 
 
@@ -53,19 +52,33 @@ def bubble_sort(number_array):
     """
     n = len(number_array)
     for i in range(n-1):
+        # pokud podmíka zde tak slozitos o(n)
         for j in range(0,n-i-1):
             if number_array[j] > number_array[j+1]:
                 number_array[j],number_array[j+1] = number_array[j+1],number_array[j]
     return number_array
-
-
+def insertion_sort(number_array):
+    """
+    :param number_array: nezerazena posloupnost cisel
+    :return: serazena posloupnost cisel
+    """
+    for i in range(1, len(number_array)):
+        key = number_array[i]
+        j = i - 1
+        while j >= 0 and key < number_array[j]:
+            number_array[j + 1] = number_array[j]
+            j -= 1
+        number_array[j + 1] = key
+    return number_array
 def main():
     data = read_data("numbers.csv")
     # print(data["series_1"])
     select = selection_sort(data["series_1"],"asc")
-    # print(select)
+    print(select)
     bubl = bubble_sort(data["series_2"])
     # print(bubl)
+    insert = insertion_sort(data["series_3"])
+    # print(insert)
     pass
 
 
