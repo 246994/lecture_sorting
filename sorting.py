694171a47d91ed5bb556d05aa023bdaab603f840
246994 <246994@vutbr.cz>
2025-04-14 09:21:19 +0200

    (select)

--- sorting.py
+++ sorting.py
@@ -21,10 +21,35 @@ def read_data(file_name):
                 else:
                     data[key].append(int(value))
     return data
+def selection_sort(seznam,direction = "asc"):
+    """
 
+    :param list seznam: numerick arrej
+    :param str direction: smer ascending nebo descengin
+    :return: sorted numeric erej
+    """
+    delka = len(seznam)
+    for i in range(delka):
+        min_indx = i
+        for j in range(i+1 ,delka):
+            if direction == "asc":
+                if seznam[j] < seznam[min_indx]:
+                    min_indx = j
+                    seznam[i], seznam[min_indx] = seznam[min_indx], seznam[i]
+            elif direction == "desc":
+                max_indx = i
+                for j in range(i+1,delka):
+                    if seznam[j] > seznam[max_indx]:
+                        max_indx = j
+                seznam[i],seznam[max_indx] = seznam[max_indx],seznam[i]
+    return seznam
 def main():
     data = read_data("numbers.csv")
-    print(data)
+
+    # print(data["series_1"])
+
+    select = selection_sort(data["series_1"],"asc")
+    print(select)
     pass
 
 
